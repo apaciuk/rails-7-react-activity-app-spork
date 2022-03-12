bfa86ab17947ec8907cf52d4fafdfb2684638b93
xhostcom <xhostcom@gmail.com>
2022-03-12 13:03:18 +0000

    Gitacora saved 6 changed files

---
+++ vue-front/src/components/TaskList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="todo in allTTodos" :key="todo.id">
+      <TaskItem :todo="todo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TaskItem from "@/components/TaskItem";
+import { mapGetters } from "vuex";
+
+export default {
+  components: {
+    TaskItem,
+  },
+  computed: {
+    ...mapGetters(["allTTodos", "completed"]),
+  },
+};
+</script>
+
+<style scoped>
+</style>
